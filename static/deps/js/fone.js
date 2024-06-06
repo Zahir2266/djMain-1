ca7d2197676559a887b2103a3cf53e42f0e7c1d7
Zahir2266 <vpapaiuny@gmail.com>
2024-06-06 18:27:15 +0700

    feat: Кнопка скрола наверх (почини чтение js файлов)

--- static/deps/js/fone.js
+++ static/deps/js/fone.js
@@ -1,19 +1,18 @@
 const asnulsabuns = document.querySelectorAll('.nav-link dropdown-toggle text-white');
-const body = document.querySelector('body');
 
 console.log(asnulsabuns);
 
-asnulsabuns.forEach(function(asnulsabun){
-  asnulsabun.addEventListener('click', function(e){
-  console.log(e.target);
-  if (e.target.id === '2'){
-  document.body.style.backgroundImage = "url(\"../images/bg-image.jpg\")";
-  }
-  if (e.target.id === '0'){
-  document.body.style.backgroundImage = "url(\'static/images/bg-image1.jpg\')";
-  }
-  if (e.target.id === '1'){
-  document.body.style.backgroundImage = "url(\"../images/bg-image2.jpg\")";
-  }
-  })
+let body = document.querySelector('body');
+const changeBgBtn = document.querySelector('.change-bg-btn');
+
+changeBgBtn.addEventListener('click', () => {
+    let index = document.body.id
+    switch (index) {
+        case '1':
+            return body.style.backgroundImage = "url('/static/deps/images/bg-image.jpg')";
+        case '2':
+            return body.style.backgroundImage = "url(\"{\%static\'\/deps\/images\/bg-image0.jpg\' %}\")";
+        case '3':
+            return body.style.backgroundImage = "url(\"{\%static\'\/deps\/images\/bg-image2.jpg\' %}\")";
+    }
 })
